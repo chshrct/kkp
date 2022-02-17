4293b395e49d7dbb22e82826097a8e7f85684e8a
Yauheni <eugene.kostroma@gmail.com>
2022-02-17 11:54:00 +0400

    kkp-components1-2

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export function Accordion() {
+    return <>
+        <AccordionTitle/>
+        <AccordionBody/>
+    </>
+}
+
+function AccordionTitle() {
+    return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Rating() {
+    return <>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+    </>
+
+}
+
+function Star() {
+    return <div>star</div>
+}
